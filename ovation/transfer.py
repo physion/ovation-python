3b1de13db823f7dd6a7277d0b84c038438465b3a
Rens Methratta <rens.methratta@gmail.com>
2016-06-17 17:09:06 -0400

    Add comments

--- ovation/transfer.py
+++ ovation/transfer.py
@@ -32,19 +32,21 @@ def copy_bucket_contents(session, project_id, aws_access_key_id, aws_secret_acce
     folder_map = {}
     parent_folder_id = project_id
 
-    for s3_file in src.objects.all():
-        is_folder = s3_file.key.endswith("/");
+    for s3_object in src.objects.all():
+
+        # e.g. s3_object.key --> 'Folder1/Folder2/Folder3/'
+        is_folder = s3_object.key.endswith("/");
 
         if is_folder:
 
-            # e.g key --> 'Folder1/Folder2/Folder3/'
-            folder_path = s3_file.key
-            folder_list = s3_file.key.split('/')
+            folder_path = s3_object.key
+            folder_list = s3_object.key.split('/')
             folder_list.remove('')
 
-            # e.g current_folder --> 'Folder3'
+            # e.g. current_folder --> 'Folder3'
             current_folder = folder_list[-1]
 
+            # if nested folder
             if len(folder_list) > 1:
                 current_folder_name = current_folder + "/"
                 parent_folder_path = find_parent_path(current_path=folder_path, current_entity_name=current_folder_name)
@@ -53,24 +55,25 @@ def copy_bucket_contents(session, project_id, aws_access_key_id, aws_secret_acce
 
                 folder_map[folder_path] = new_folder._id
             else:
+                # folder is at project root (project_id is the parent_id)
                 new_folder = core.create_folder(session, project_id, current_folder)
                 folder_map[folder_path] = new_folder._id
 
         else:
+            # s3_object is a file
+            file_path = s3_object.key
+            path_list = s3_object.key.split('/')
 
-            file_path = s3_file.key
-            path_list = s3_file.key.split('/')
-
-            # e.g current_file --> 'test.png'
-            current_file = path_list[-1]
+            # e.g file_name --> 'test.png'
+            file_name = path_list[-1]
             parent_folder_id = project_id
 
             if len(path_list) > 1:
-                parent_folder_path = find_parent_path(current_path=file_path, current_entity_name=current_file)
+                parent_folder_path = find_parent_path(current_path=file_path, current_entity_name=file_name)
                 parent_folder_id = folder_map[parent_folder_path]
 
             # create revision
-            create_file(session=session, file_key=file_path, file_name=current_file, parent_folder_id=parent_folder_id, source_bucket=source_s3_bucket, destination_bucket=destination_s3_bucket, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+            create_file(session=session, file_key=file_path, file_name=file_name, parent_folder_id=parent_folder_id, source_bucket=source_s3_bucket, destination_bucket=destination_s3_bucket, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
 def find_parent_path(current_path, current_entity_name):
     """
@@ -82,12 +85,16 @@ def find_parent_path(current_path, current_entity_name):
 
 def create_file(session, parent_folder_id, file_key, file_name, source_bucket, destination_bucket, aws_access_key_id, aws_secret_access_key):
     """
-    Upload a new `Revision` to `parent_file`. File is uploaded from `local_path` to
+    Creates an Ovation 'File' and 'Revision' record.   a new `Revision` to `parent_file`. File is uploaded from `local_path` to
     the Ovation cloud, and the newly created `Revision` version is set.
     :param session: ovation.connection.Session
-    :param parent_file: file entity dictionary or file ID string
-    :param local_path: local path
-    :param progress: if not None, wrap in a progress (i.e. tqdm). Default: tqdm
+    :param parent_folder_id:
+    :param file_key: S3 Key of file to copy from source bucket
+    :param file_name: Name of file
+    :param source_bucket: the S3 bucket to copy file from
+    :param destination_bucket: the destination_bucket to copy file to
+    :param aws_access_key_id: id for key that must have read access to source and write access to destination
+    :param aws_secret_access_key: AWS key
     :return: new `Revision` entity dicitonary
     """
 
@@ -95,29 +102,33 @@ def create_file(session, parent_folder_id, file_key, file_name, source_bucket, d
     if content_type is None:
         content_type = 'application/octet-stream'
 
+    # create file record
     new_file = core.create_file(session, parent_folder_id, file_name)
 
+    # create revision record
     r = session.post(new_file['links']['self'],
                      data={'entities': [{'type': 'Revision',
                                          'attributes': {'name': file_name,
                                                         'content_type': content_type}}]})
-    revision = r['entities'][0]
-    aws = r['aws'][0]['aws']
 
+    # get key for where to copy exisitng file from create revision resonse
+    revision = r['entities'][0]
+    destination_s3_key = r['aws'][0]['aws']['key']
+
+    # Copy file from source bucket to destination location
+    # Need to use key that has 'list' and 'get_object' privleges on source bucket as well as 'write' privleges
+    # on destination bucket (can't use the temp aws key from the create revision response since this does
+    # not have read access to the source bucket)
     aws_session = boto3.Session(aws_access_key_id=aws_access_key_id,
                                 aws_secret_access_key=aws_secret_access_key)
 
     s3 = aws_session.resource('s3')
-
-    destination_file = s3.Object(destination_bucket, aws['key'])
-
+    destination_file = s3.Object(destination_bucket, destination_s3_key)
     copy_source = "{0}/{1}".format(source_bucket, file_key)
-    #client.copy_object(Bucket=destination_bucket, CopySource=copy_source, Key=file_name)
+    aws_response = destination_file.copy_from(CopySource=copy_source)
 
-    response = destination_file.copy_from(CopySource=copy_source)
-
-    revision['attributes']['version'] = response['VersionId']
-
+    # get version_id from AWS copy response and update revision record with aws version_id
+    revision['attributes']['version'] = aws_response['VersionId']
     revision_response = session.put('/api/v1/revisions/{}'.format(revision['_id']), entity=revision)
 
     return revision_response
