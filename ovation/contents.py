e14e2f49723d407eacedbbe6f87cac85921bab69
Barry Wark <barrywark@gmail.com>
2016-07-11 14:16:00 -0400

    Progress for head revisions

--- ovation/contents.py
+++ ovation/contents.py
@@ -1,9 +1,12 @@
+import functools
 import texttable
-import six
 
 import ovation.core as core
 
 from pprint import pprint
+from tqdm import tqdm
+from multiprocessing.pool import ThreadPool as Pool
+
 
 def get_contents(session, parent):
     """
@@ -19,6 +22,11 @@ def get_contents(session, parent):
 
     return {'files': session.get(p.relationships.files.related),
             'folders': session.get(p.relationships.folders.related)}
+
+
+def _get_head(session, file):
+    return {'file': file._id,
+            'revision': session.get(file.links.heads)[0]._id}
 
 
 def list_contents_main(args):
@@ -39,23 +47,31 @@ def list_contents_main(args):
 
     else:
         contents = get_contents(session, parent_id)
-        contents = contents['files'] + contents['folders']
+        files = contents['files']
+        folders = contents['folders']
+
+        revisions = {}
+        with Pool() as pool:
+            for r in tqdm(pool.imap_unordered(functools.partial(_get_head, session), files),
+                          desc='Finding HEAD revisions',
+                          unit=' file',
+                          total=len(files)):
+                revisions[r['file']] = r['revision']
+
 
         table = texttable.Texttable()
         table.set_deco(texttable.Texttable.HEADER)
-        table.set_cols_align(['l','l','l'])
+        table.set_cols_align(['l', 'l', 'l', 'l'])
         # table.set_cols_width([])
-        table.add_rows([['Name', 'Modified', 'UUID']])
-        contents.sort(key=lambda e: e.attributes.name)
-        for e in contents:
+        table.add_rows([['Name', 'Modified', 'ID', 'HEAD']])
+        for e in sorted(files + folders, key=lambda e: e.attributes.name):
             if e.type == core.FOLDER_TYPE:
                 name = e.attributes.name + "/"
+                head = ''
             else:
                 name = e.attributes.name
+                head = revisions[e._id]
 
-            table.add_row([name, e.attributes['updated-at'], e._id])
+            table.add_row([name, e.attributes['updated-at'], e._id, head])
 
-        pprint(table.__dict__)
         print(table.draw())
-
-
