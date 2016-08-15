5b35f8a4ee4cb9232d034472acbebb886ab235d5
Barry Wark <barrywark@gmail.com>
2016-08-15 10:21:28 -0400

    WIP

--- test/test_workflows.py
+++ test/test_workflows.py
@@ -9,14 +9,14 @@ from nose.tools import istest, assert_equal
 def should_create_activity():
     label = 'activity-label'
     workflow_id = 1
-    workflow = {'links': {label: {'self': sentinel.activity_url}}}
+    workflow = {'relationships': {label: {'self': sentinel.activity_url}}}
 
     s = Mock(spec=Session)
     s.entity_path.return_value = sentinel.workflow_path
     s.get.return_value = simplify_response({'workflow': workflow})
     s.post.return_value = sentinel.activity
 
-    workflows.create_activity(s, workflow_id, label, data=sentinel.data)
+    workflows.create_activity(s, workflow_id, label, activity=sentinel.data)
 
     s.entity_path.assert_called_with('workflows', workflow_id)
     s.get.assert_called_with(sentinel.workflow_path)
