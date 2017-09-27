6b3ad9f3faffaef43fb84f9b2f79b838caacec50
Barry Wark <barrywark@gmail.com>
2017-09-27 10:20:22 -0400

    Use twine for upload

--- deploy_pypi.sh
+++ deploy_pypi.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 
-FILE=~/.pypirc
-echo > $FILE
-echo "[distutils]" >> $FILE
-echo "index-servers = pypi" >> $FILE
-echo "[pypi]" >> $FILE
-echo "username:$PYPI_USERNAME" >> $FILE
-echo "password:$PYPI_PASSWORD" >> $FILE
-
-python setup.py sdist upload
+python setup.py sdist
+twine -u $PYPI_USERNAME -p $PYPI_PASSWORD upload dist/*
