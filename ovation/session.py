bacf36bac0508f95f3c8fc430f1830bf9a9cf49c
tamaraOIntegradevs <tamara+ovation@integradevs.com>
2018-01-16 10:58:46 -0300

    add dns name

--- ovation/session.py
+++ ovation/session.py
@@ -16,7 +16,7 @@ from getpass import getpass
 
 DEFAULT_HOST = 'https://api.ovation.io'
 DEFAULT_LAB_HOST = 'https://lab-services.ovation.io'
-DEVELOPMENT_HOST = 'http://35.201.81.129'
+DEVELOPMENT_HOST = 'http://api-dev.ovation.io'
 
 
 class DataDict(dict):
